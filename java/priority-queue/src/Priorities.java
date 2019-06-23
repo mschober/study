b3f7dbb19b853c7cdef41117ac7e1fb8122882a9
Michael Schober <mike@getmigo.com>
2019-06-23 16:40:54 -0700

    able to add nodes

--- java/priority-queue/src/Priorities.java
+++ java/priority-queue/src/Priorities.java
@@ -54,28 +54,96 @@ public class Priorities {
 }
 
 class Node {
-    private Node next;
-    private Student student;
+    Node next;
+    Student student;
 
     public Node(Student student) {
         this.next = null;
         this.student = student;
     }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "next=" + next +
+                ", student=" + student +
+                '}';
+    }
 }
 
 class PQ {
 
+    private Node root;
     private ArrayList<Student> students;
 
     public PQ() {
-        this.students = new ArrayList<Student>();
+        this.root = null;
     }
 
     void enqueue(Student student) {
-        this.students.add(student);
+        if (this.root != null) {
+            System.out.println("going to insert student " + student);
+            this.insert(student);
+        }
+        else {
+            System.out.println("initializing pq " + student);
+            this.root = new Node(student);
+        }
+        System.out.println("pq after enqueue " + this);
+    }
+
+    void insert(Student student) {
+        Node curr = this.root;
+        boolean inserted = false;
+        while(!inserted) {
+            int comparison = curr.student.compareTo(student);
+            System.out.println("comparison of students " + comparison);
+            if (comparison < 0) {
+                System.out.println("going to next");
+                curr = curr.next;
+            }
+            else {
+                System.out.println("going to insert " + student);
+                curr.next = new Node(student);
+                inserted = true;
+            }
+            if (curr == null) {
+                break;
+            }
+        }
     }
 
     Student dequeue() {
-        return this.students.remove(this.students.size() - 1);
+        Student popped = this.pop();
+        System.out.println("pq after dequeue " + this);
+        return popped;
+    }
+
+    private Student pop() {
+        Node temp;
+        Student popped;
+        if (this.root.next != null) {
+            temp = this.root.next;
+        }
+        else {
+            temp = null;
+        }
+        popped = this.root.student;
+        this.root = temp;
+        return popped;
+    }
+
+    @Override
+    public String toString() {
+        String toPrint = "";
+        Node curr = this.root;
+        toPrint += curr.toString();
+        System.out.println("root is " + root);
+        while(curr.next != null) {
+            curr = curr.next;
+            toPrint += curr.toString();
+            System.out.println("toPrint is " + toPrint);
+        }
+        return toPrint;
     }
 }
--- java/priority-queue/src/Student.java
+++ java/priority-queue/src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Student  {
     private int id;
     private String name;
@@ -30,4 +32,30 @@ public class Student  {
         return this.cgpa;
     }
 
+    /**
+     *
+     * The student having the highest Cumulative Grade Point Average (CGPA) is served first.
+     * Any students having the same CGPA will be served by name in ascending case-sensitive alphabetical order.
+     * Any students having the same CGPA and name will be served in ascending order of the id.
+     */
+    public int compareTo(Student that) {
+        if (this.cgpa > that.cgpa) {
+            return -1;
+        }
+        else if (this.cgpa < that.cgpa) {
+            return 1;
+        }
+        if (this.name.compareTo(that.name) > 0) {
+            return -1;
+        }
+        else if (this.name.compareTo(that.name) < 0) {
+            return 1;
+        }
+        if (this.id > that.id) {
+            return -1;
+        }
+        else {
+            return 1;
+        }
+    }
 }
