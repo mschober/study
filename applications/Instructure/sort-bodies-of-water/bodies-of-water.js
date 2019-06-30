1b1aa246509a31f2c34bb432f68df9eab95a628a
Michael Schober <mike@getmigo.com>
2019-06-29 19:43:24 -0700

    going to remove visited

--- applications/Instructure/sort-bodies-of-water/bodies-of-water.js
+++ applications/Instructure/sort-bodies-of-water/bodies-of-water.js
@@ -28,7 +28,7 @@ function readLine() {
 }
 
 
-function countBodies(arr, pos, bodySize, bodies, visited) {
+function countBodies(arr, pos, bodySize, visited) {
   function inBounds() {
     let [x,y] = pos;
     return (x >= 0 && y >= 0 && x < arr.length && y < arr.length);
@@ -37,32 +37,33 @@ function countBodies(arr, pos, bodySize, bodies, visited) {
   function isNewWaterLocation() {
     let [x,y] = pos;
     let curr = arr[x][y];
-    console.log('curr is', curr, visited, !visited[x][y]);
     let shouldRecur = (curr === 0 && !visited[x][y]);
     visited[x][y] = true;
-    console.log('should recur', shouldRecur);
+    // console.log('should recur', shouldRecur);
     return shouldRecur;
   }
 
   function recurInAllDirections() {
     let [x,y] = pos;
     bodySize++;
-    console.log('bodySize is', bodySize);
+    console.log('pos is', pos, bodySize, visited, !visited[x][y]);
     let directions = [
       [x-1,y],
+      [x-1,y+1],
       [x,y+1],
+      [x+1,y+1],
       [x+1,y],
-      [x,y-1]
+      [x+1,y-1],
+      [x,y-1],
+      [x-y,y-1]
     ]
     for (let thisPos of directions) {
-      bodies.push(countBodies(arr, thisPos, bodySize, bodies, visited));
+      return countBodies(arr, thisPos, bodySize, visited);
     }
-    console.log('done with body of water');
-    return bodySize;
   }
 
   if (!inBounds() || !isNewWaterLocation()) {
-    return;
+    return bodySize;
   }
   else {
     return recurInAllDirections();
@@ -77,7 +78,12 @@ function sortCountedBodies(arr) {
   }
   console.log('visited is', visited);
   let bodies = [];
-  countBodies(arr, [0,0], 0, bodies, visited);
+  for (let col = 0; col < arr.length; col++) {
+    for (let row = 0; row < arr.length; row++) {
+      bodies.push(countBodies(arr, [row, col], 0, visited));
+    }
+  }
+  // countBodies(arr, [0,0], 0, bodies, visited);
   // console.log('bodies are', bodies);
   return bodies;
 }
